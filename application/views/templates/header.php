30cd09737ef6d916dec6f4bf3e0241a5ddebaf76
dikyanwar22 <dikyanwar22@gmail.com>
2026-09-21 15:21:38 +0700

    teks header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -19,6 +19,7 @@
     <button class="btn btn-dark d-lg-none ms-2" id="mobileMenuToggle">
         <i class="bi bi-list fs-4"></i>
     </button>
+    <span class="erp-system-mobile d-flex d-lg-none align-items-center ms-2 me-auto">ERP System</span>
     <div class="topnav-wrapper flex-grow-1 mx-3">
         <button class="scroll-btn scroll-left d-none" id="scrollLeft" aria-label="Scroll left">
             <i class="bi bi-chevron-left"></i>
